8bd05f12151e05679a331ccf1a873b597a46359d
Ayush <ayush.sindhwani@impactguru.com>
2019-06-28 16:37:51 +0530

    Updates

---
+++ precache-manifest.640889ed93c33a1fcc4b4648e4af8b0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e69d70446ca0ab14594b68ad9e6cf09",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "397116f6a375969a4d7a",
+    "url": "/currency-converter/static/css/main.4d3e32f4.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/currency-converter/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "397116f6a375969a4d7a",
+    "url": "/currency-converter/static/js/main.11e8032f.chunk.js"
+  },
+  {
+    "revision": "3fb806550804c3de195a",
+    "url": "/currency-converter/static/js/runtime~main.ac68a149.js"
+  }
+]);
